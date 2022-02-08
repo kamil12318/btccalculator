3923fe7991fa113d3a61d1be1b7603daa70cbcee
kamil12318 <kamil123181@o2.pl>
2022-02-08 12:03:31 +0100

    main paga v0.2 working buttons

--- app/src/main/java/com/example/btccalculator/MainActivity.java
+++ app/src/main/java/com/example/btccalculator/MainActivity.java
@@ -4,15 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private MaterialCardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardView = findViewById(R.id.cVkrypto);
+        cardView.setOnClickListener(v -> Toast.makeText(MainActivity.this, "krypto clicked", Toast.LENGTH_SHORT).show());
+
+        cardView = findViewById(R.id.cVklasyczne);
+        cardView.setOnClickListener(v -> Toast.makeText(MainActivity.this, "klasyczne clicked", Toast.LENGTH_SHORT).show());
+
+        cardView = findViewById(R.id.cVcredits);
+        cardView.setOnClickListener(v -> Toast.makeText(MainActivity.this, "Credits clicked", Toast.LENGTH_SHORT).show());
+
+
+
+
+
+
+
     }
+
 
 }
