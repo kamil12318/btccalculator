a05e44a9498f5cd9e18afb6ef454412f4f79f491
kamil12318 <kamil123181@o2.pl>
2022-02-09 19:06:03 +0100

    final

--- app/src/main/java/com/example/btccalculator/kalkulator.java
+++ app/src/main/java/com/example/btccalculator/kalkulator.java
@@ -3,6 +3,7 @@ package com.example.btccalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class kalkulator extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class kalkulator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kalkulator);
+    }
+    private void configureBackButton(){
+        Button btnBack = (Button) findViewById(R.id.btnback);
+        btnBack.setOnClickListener(v -> finish());
     }
 }
