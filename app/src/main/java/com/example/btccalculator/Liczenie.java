b8a950a22c5649ce07a258177d250140dc91b30e
kamil12318 <kamil123181@o2.pl>
2022-02-09 03:08:32 +0100

    main page v0.3 working buttons credits done added icons and another page

---
+++ app/src/main/java/com/example/btccalculator/Liczenie.java
@@ -0,0 +1,10 @@
+package com.example.btccalculator;
+
+public class Liczenie {
+    int dzielna;
+    int dzielnik;
+    int wynik;
+    double kurs;
+    int waluta;
+
+}
--- app/src/main/java/com/example/btccalculator/kalklasyczne.java
+++ app/src/main/java/com/example/btccalculator/kalklasyczne.java
@@ -1,14 +1,99 @@
 package com.example.btccalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.material.card.MaterialCardView;
+
+import java.util.ArrayList;
 
 public class kalklasyczne extends AppCompatActivity {
+
+    Liczenie Euro = new Liczenie();
+
+    private MaterialCardView cardViewEUR;
+    private MaterialCardView cardViewSF;
+    private MaterialCardView cardViewUSD;
+    private MaterialCardView cardViewPLN;
+    private MaterialCardView cardViewGBP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kalklasyczne);
+
+        cardViewSF = findViewById(R.id.sf);
+        cardViewEUR = findViewById(R.id.cVeur);
+
+
+
+        cardViewEUR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(kalklasyczne.this, "sdfasdfsadfsd", Toast.LENGTH_SHORT).show();
+                Euro.kurs = 0.88;
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+        Spinner listawalut = findViewById(R.id.spinner);
+
+        ArrayList<String> waluty = new ArrayList<>();
+        waluty.add("Euro");
+        waluty.add("Dolar");
+        waluty.add("Frank");
+        waluty.add("Funt");
+        waluty.add("Złoty");
+
+        ArrayAdapter<String> walutyAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                waluty
+        );
+        listawalut.setAdapter(walutyAdapter);
+
+
+        listawalut.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                switch((int) position){
+                    case 0:
+
+                    case 1:
+                        Toast.makeText(kalklasyczne.this, " asdfasdfsdafsadfsdafsadf", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+
+
+
+
     }
+
 }
